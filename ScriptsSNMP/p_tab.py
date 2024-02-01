5de01a6525a38116eefde6fffc3b63448099b71a
Edwix01 <edwinlima201@gmail.com>
2024-02-01 13:25:17 -0800

    Actualizacion de Scripts SNMP

--- ScriptsSNMP/p_tab.py
+++ ScriptsSNMP/p_tab.py
@@ -9,12 +9,12 @@ def display_interface_table(interface_dict):
 
     print(table)
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # Ejemplo de un diccionario con direcciones IP y sus interfaces activas
-    ip_interface_dict = {
-        '192.168.1.1': ['eth0', 'eth1'],
-        '192.168.1.2': ['eth2'],
-        '192.168.1.3': ['eth3', 'eth4']
-    }
+#    ip_interface_dict = {
+#        '192.168.1.1': ['eth0', 'eth1'],
+#        '192.168.1.2': ['eth2'],
+#        '192.168.1.3': ['eth3', 'eth4']
+#    }
 
-    display_interface_table(ip_interface_dict)
+
