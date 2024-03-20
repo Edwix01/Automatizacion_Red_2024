ca394517e1c41b1a807afa6527f5a4c1b9ebd793
Edwix01 <edwinlima201@gmail.com>
2024-03-19 21:09:24 -0500

    Adición de Comentarios

--- Scriptsv2/mainv2.py
+++ Scriptsv2/mainv2.py
@@ -4,68 +4,46 @@ import com_conex
 import des_disp
 import des_int_act
 import gr
-import networkx as nx
-import matplotlib.pyplot as plt
-import random
 import map_int
 import stp_blk
 import verstp
-import matplotlib.colors as mcolors
+
+
+# Ingreso de Parametros - Comunidad SNMP, Direcciones IP
 
 comunidad = "$1$5.v/c/$"
 direc = des_disp.in_des()
-#Descubrir Interfaces activas 
+
+#Detección de interfaces activas 
 d = (des_int_act.in_act(direc,comunidad))
-direc = d.keys()
+direc = d.keys() #Direcciones IP Filtradas
 
 info_int = map_int.ma_int(direc,comunidad)
 
-
+#Informacion STP
+# Bridge ID, Designed Bridge
 b_id = bridge_id.bri_id(direc,comunidad)
-
 st_inf = stp_info.stp_inf(direc,comunidad)
 
+#Identificación de Conexiones
 l = com_conex.b_conex(direc,b_id,st_inf)
 
-
-# Crear una lista para almacenar los primeros números antes del punto
+#Filtro de Switches sin conexiones
+"""
 l1 = []
-
-# Recorrer todas las tuplas y dentro de cada tupla obtener el primer número antes del punto
 for tupla in l:
     for elemento in tupla:
         nodo_principal = elemento.split('.')[0]
         if nodo_principal not in l1:
             l1.append(nodo_principal)
+"""
 
 
-# Define los colores para los nodos
-# Definir colores pastel predefinidos
-colores_pastel = {
-    'pastel_blue': mcolors.CSS4_COLORS['lightblue'],    # Azul pastel
-    'pastel_green': mcolors.CSS4_COLORS['lightgreen'],  # Verde pastel
-    'pastel_yellow': mcolors.CSS4_COLORS['lightcoral'] # Amarillo pastel
-}
-
-# Reemplazar los colores en colores_nodos con colores pastel
-colores_nodos= {
-    'principal': colores_pastel['pastel_blue'],   # Azul pastel para nodos principales
-    'subnodo': colores_pastel['pastel_green'],    # Verde pastel para subnodos
-    'subnodob': colores_pastel['pastel_yellow']   # Amarillo pastel para subnodos
-}
-
+#Deteccion de puertos habilitados con stp
+#Detección de puertos bloqueados por stp
 nf = verstp.obtener_numeros_despues_del_punto(l)
-
 nodb=stp_blk.stp_status(direc,nf,comunidad)
 
-# Crear el grafo
-grafo = gr.crear_grafo(l1, l, info_int, colores_nodos,nodb)
-
-# Determinar tamaños de los nodos individualmente
-tamanos_nodos = [grafo.nodes[nodo]['size'] for nodo in grafo.nodes()]
-
-# Dibujar el grafo con posiciones fijas utilizando el algoritmo Kamada-Kawai
-posiciones = nx.spring_layout(grafo,seed=42)
-nx.draw(grafo, pos=posiciones, with_labels=True, labels=nx.get_node_attributes(grafo, 'label'), node_size=tamanos_nodos, node_color=[grafo.nodes[nodo]['color'] for nodo in grafo.nodes()], font_size=8, font_weight='bold',font_family="serif")
-
-plt.show()
+#Creación del grafo
+grafo = gr.crear_grafo(direc, l, info_int,nodb)
+gr.dibujar_grafo(grafo)
