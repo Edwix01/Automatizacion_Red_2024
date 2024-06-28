8686825bdece93cc1821367f9f33ea6b9b69daab
Edwix01 <edwinlima201@gmail.com>
2024-06-28 12:25:08 -0500

    Se borraron archivos - Nuevo Uso

--- app_2024/modulo_automatizacion/read_yaml.py
+++
@@ -1,5 +0,0 @@
-import yaml
-
-def cargar_configuracion_yaml(ruta_archivo):
-    with open(ruta_archivo, 'r') as archivo:
-        return yaml.safe_load(archivo)
--- tarea1/rv_sumo.sh
+++
@@ -1,1 +0,0 @@
-/home/paola/Documentos/Programs/veins-veins-5.2/bin/veins_launchd -vv -c /home/paola/Documentos/Programs/sumo-1_7_0/bin/sumo
