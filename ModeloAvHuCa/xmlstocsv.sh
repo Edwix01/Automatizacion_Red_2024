1d11ff72a4b89a44ba28eb9392b5faa6c0895eae
Edwix01 <edwinlima201@gmail.com>
2024-07-11 17:16:45 -0500

    Se borraron archivos

--- ModeloAvHuCa/xmlstocsv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Comando para convertir XML a CSV
-python3 /opt/sumo/tools/xml/xml2csv.py -s , fcd2.xml
-python3 /opt/sumo/tools/xml/xml2csv.py -s , tripinfo2.xml
-
-# Mensaje para indicar que el script ha terminado
-echo "Conversión de XML a CSV completada."
-
